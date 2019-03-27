8c4ded48f34d79cf57cd5463703c51be993d6bab
anjelicakay <aktowner@gmail.com>
2019-03-27 00:59:48 -0500

    added scope to inventory model

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,7 +1,7 @@
 class Inventory < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  validates :book_id, uniqueness: true
+  validates :book_id, uniqueness: {scope: :user_id, message: "should only add a book once."}
 
   enum status: {currently_reading: 0, want_to_read: 1, read: 2}
 end
